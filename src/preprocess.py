9d36d1ccbdc70e1af9f55969d1041ec16c0b77a4
Prawesh Rai <prai82804@gmail.com>
2026-06-16 13:29:19 +0530

    Day 8 preprocessing completed

---
+++ src/preprocess.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import string
+import nltk
+
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+
+nltk.download("stopwords")
+
+# Load dataset
+import os
+
+dataset_path = os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "data",
+    "SMSSpamCollection"
+)
+
+df = pd.read_csv(
+    dataset_path,
+    sep="\t",
+    header=None,
+    names=["label", "message"]
+)
+
+# Convert text to lowercase
+df["message"] = df["message"].str.lower()
+
+# Remove punctuation and stopwords
+stop_words = set(stopwords.words("english"))
+
+def clean_text(text):
+    text = "".join(
+        char for char in text
+        if char not in string.punctuation
+    )
+
+    words = text.split()
+
+    words = [
+        word for word in words
+        if word not in stop_words
+    ]
+
+    return " ".join(words)
+
+df["message"] = df["message"].apply(clean_text)
+
+# Tokenization
+df["tokens"] = df["message"].apply(lambda x: x.split())
+
+# Convert labels
+df["label"] = df["label"].map({
+    "ham": 0,
+    "spam": 1
+})
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(
+    df["message"],
+    df["label"],
+    test_size=0.2,
+    random_state=42
+)
+
+print("Training samples:", len(X_train))
+print("Testing samples:", len(X_test))
+print(df.head())
